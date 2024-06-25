23264f1d285680f95d61de2490800a5c6ce50596
Rafik TAAMMA <baghdad-rafik.taamma@eviden.com>
2024-06-25 10:50:40 +0200

    fixing the config file to populate from the environement varaibles

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,3 +1,2 @@
-const API_BASE_URL = "http://localhost:8080";
-// const API_BASE_URL = "http://172.20.5.246:80";
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8080";
 export default API_BASE_URL;
